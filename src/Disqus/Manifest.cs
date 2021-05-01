6b270032d730aa4006cd806119417d0626a61ad8
Antoine Griffard <agriffard@hotmail.com>
2021-05-01 12:02:57 +0200

    16160

--- src/Disqus/Manifest.cs
+++ src/Disqus/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "Disqus",
     Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/Disqus",
+    Website = "https://github.com/agriffard/Disqus.OrchardCore",
     Version = "1.0.0",
     Description = "The Disqus module allows you to add a comments section.",
     Category = "Comments"
